3d2671be1f1576697fd03e36be3f2ef99393af60
MrHarrys <nikitarudoy98@gmail.com>
2024-04-08 23:21:50 +0300

    fixed code, added dump logic to find pair brackets.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,6 +11,8 @@ public class Main {
     static String correctString;
     static int numberOfOpenBracket;
     static int numberOfCloseBracket;
+    static List<String> positionOfPairedBrackets = new ArrayList<>();
+    static String bufferString;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -27,10 +29,31 @@ public class Main {
                 numberOfCloseBracket++;
         }
 
-        System.out.println("Number of open brackets - " + numberOfOpenBracket + ".\nNumber of close brackets - " + numberOfCloseBracket);
+        System.out.println("Number of open brackets - " + numberOfOpenBracket + ".\nNumber of close brackets - " + numberOfCloseBracket + ".\nCorrect string is - " + correctString);
 
+        processBrackets(correctString, 1);
+    }
 
-
+    private static String processBrackets(String string, int position) {
+        if (position < string.length()) {
+            if (string.charAt(position) == openBracket) {
+                processBrackets(string, position + 1);
+            }
+            else if (string.charAt(0) == closeBracket){
+                processBrackets(string.substring(1), 1);
+            } else {
+                System.out.println("Pair bracket founded - positions are " + (position - 1) + " and " + position);
+                positionOfPairedBrackets.add(String.valueOf((position - 1) + String.valueOf(position)));
+                if (position > 1) {
+                    bufferString = string.substring(0, position - 1) + string.substring(position);
+                } else {
+                    bufferString = string.substring(position);
+                }
+                System.out.println("String is - " + bufferString);
+                processBrackets(string, position + 1);
+            }
+        }
+        return string;
     }
 
     private static String removedUnusedChars(String string) {
