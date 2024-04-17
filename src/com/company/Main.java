69f8f497d8dabfcd671810ddff8bd3c839956ab0
MrHarrys <nikitarudoy98@gmail.com>
2024-04-17 23:43:38 +0300

    fixed logic, final version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -36,11 +36,27 @@ public class Main {
 
         processInnerBrackets(correctString, 0, 1, true);
 
-        System.out.println("TEST " + positionOfPairedBrackets + " STRING " + bufferString);
+//        System.out.println("TEST " + positionOfPairedBrackets + "\nSTRING " + bufferString);
 
         bufferString = replacedIncorrectStartAndEndBrackets(bufferString);
 
-        System.out.println("AFTER ALL " + bufferString);
+        if (bufferString.contains(stringOpenBracket) || bufferString.contains(stringCloseBracket))
+            processFinalString(bufferString, 0, 0);
+
+//        System.out.println("AFTER ALL " + bufferString + "\n" + positionOfPairedBrackets);
+
+        StringBuilder correctBracketsString = new StringBuilder();
+        correctBracketsString.setLength(positionOfPairedBrackets.size());
+
+        for (String element : positionOfPairedBrackets){
+            int openBracketPos = Integer.parseInt(element.substring(0, (element.indexOf("_"))));
+            int closeBracketPos = Integer.parseInt(element.substring((element.indexOf("_") + 1)));
+
+            correctBracketsString.insert(openBracketPos, stringOpenBracket);
+            correctBracketsString.insert(closeBracketPos, stringCloseBracket);
+        }
+
+        System.out.println("FINALLY - " + correctBracketsString.toString().replaceAll("[^{}]",""));
     }
 
     private static String processInnerBrackets(String string, int position, int lengthOfBufChar, boolean boolMarker) {
@@ -62,7 +78,7 @@ public class Main {
                     bufferString += "a";
                 }
                 bufferString += string.substring(position + 1);
-                System.out.println("String is - " + bufferString);
+//                System.out.println("String is - " + bufferString);
                 processInnerBrackets(bufferString, position + 1, lengthOfBufChar, false);
             }
         }
@@ -89,13 +105,46 @@ public class Main {
     private static String replacedIncorrectStartAndEndBrackets(String string) {
         boolean isAllBracketsDone = false;
         do {
-            if (string.indexOf(stringOpenBracket) > string.indexOf(stringCloseBracket)) {
-                string = string.substring(0, string.indexOf(stringCloseBracket)) + "a" + string.substring(string.indexOf(stringCloseBracket) + 1);
-            } else if (string.lastIndexOf(stringOpenBracket) > string.lastIndexOf(stringCloseBracket)) {
-                string = string.substring(0, string.lastIndexOf(stringOpenBracket)) + "a" + string.substring(string.lastIndexOf(stringOpenBracket) + 1);
-            } else isAllBracketsDone = true;
+            if (string.indexOf(stringOpenBracket) > 0 && string.indexOf(stringCloseBracket) > 0) {
+                if (string.indexOf(stringOpenBracket) > string.indexOf(stringCloseBracket)) {
+                    string = string.substring(0, string.indexOf(stringCloseBracket)) + "a" + string.substring(string.indexOf(stringCloseBracket) + 1);
+                } else if (string.lastIndexOf(stringOpenBracket) > string.lastIndexOf(stringCloseBracket)) {
+                    string = string.substring(0, string.lastIndexOf(stringOpenBracket)) + "a" + string.substring(string.lastIndexOf(stringOpenBracket) + 1);
+                } else isAllBracketsDone = true;
+            } else {
+                if (string.indexOf(stringOpenBracket) > 0) {
+                    string = string.substring(0, string.indexOf(openBracket)) + string.substring(string.lastIndexOf(openBracket));
+                } else if (string.indexOf(stringCloseBracket) > 0) {
+                    string = string.substring(0, string.indexOf(closeBracket)) + string.substring(string.lastIndexOf(closeBracket));
+                }
+                isAllBracketsDone = true;
+            }
         } while (!isAllBracketsDone);
 
+        return string;
+    }
+
+    private static String processFinalString(String string, int position, int posOfOpenBracket){
+         if (position < string.length()){
+            if (string.charAt(position) == 'a') {
+                processFinalString(string, position + 1,  posOfOpenBracket);
+            }
+            else if (string.charAt(position) == openBracket){
+                processFinalString(string, position + 1, position);
+            }
+            else if (string.charAt(position) == closeBracket){
+                int lengthOfBufChar = position - posOfOpenBracket;
+                System.out.println("Pair bracket founded - positions are " + (position - lengthOfBufChar) + " and " + position);
+                positionOfPairedBrackets.add((position - lengthOfBufChar) + "_" + position);
+                bufferString = string.substring(0, position - lengthOfBufChar);
+                for (int i = 0; i <= lengthOfBufChar; i++) {
+                    bufferString += "a";
+                }
+                bufferString += string.substring(position + 1);
+//                System.out.println("String is - " + bufferString);
+                processFinalString(bufferString, 0, 0);
+            }
+        }
         return string;
     }
 }
